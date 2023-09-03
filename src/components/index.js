ae3b89a24903246defbeb657c67669b6e1c2da48
Aaron-Muuo <aaronmuuo60@gmail.com>
2023-09-03 18:54:28 +0300

    Added sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,5 @@ export { default as Feed } from './Feed';
 export { default as ChannelDetail } from './ChannelDetail';
 export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
+export { default as SearchInput } from './SearchInput';
+export { default as Sidebar } from './Sidebar';
